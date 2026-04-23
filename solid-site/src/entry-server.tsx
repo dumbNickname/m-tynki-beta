53f3786007c2005f9a14141140391225d6651ca2
pietrowski.barcode <pietrowski.barcode@gmail.com>
2026-04-23 06:54:49 +0000

    Lets preload custom fonts as well, but I think I should drop those in future

--- solid-site/src/entry-server.tsx
+++ solid-site/src/entry-server.tsx
@@ -14,6 +14,12 @@ export default createHandler(() => (
           <link rel="icon" href="images/mtynk-ludzik-gladziolit-300x300-1-150x150.png" sizes="32x32" />
           <link rel="icon" href="images/mtynk-ludzik-gladziolit-300x300-1.png" sizes="192x192" />
           <link rel="apple-touch-icon" href="images/mtynk-ludzik-gladziolit-300x300-1.png" />
+          <link rel="preload" as="font" type="font/woff2" href="fonts/inter-400-latin.woff2" crossorigin="" />
+          <link rel="preload" as="font" type="font/woff2" href="fonts/inter-400-latin-ext.woff2" crossorigin="" />
+          <link rel="preload" as="font" type="font/woff2" href="fonts/inter-600-latin.woff2" crossorigin="" />
+          <link rel="preload" as="font" type="font/woff2" href="fonts/inter-600-latin-ext.woff2" crossorigin="" />
+          <link rel="preload" as="font" type="font/woff2" href="fonts/plus-jakarta-sans-600-latin.woff2" crossorigin="" />
+          <link rel="preload" as="font" type="font/woff2" href="fonts/plus-jakarta-sans-600-latin-ext.woff2" crossorigin="" />
           <link rel="preload" as="image" href="images/hero-768w.jpg" imagesrcset="images/hero-480w.jpg 480w, images/hero-768w.jpg 768w, images/hero-1125w.jpg 1125w" imagesizes="100vw" fetchpriority="high" />
           <script>{`(function(){try{var t=localStorage.getItem("mtynk_theme");if(t==="dark"||(!t&&window.matchMedia("(prefers-color-scheme:dark)").matches))document.documentElement.setAttribute("data-theme","dark")}catch(e){}})()`}</script>
           {assets}
